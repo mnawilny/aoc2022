fc71379a6f94abdfaeeb3a4767b5e99468b190bb
michal.nawilny <michal.nawilny@tesco.com>
2022-12-25 07:18:21 +0100

    day25

--- src/main/kotlin/com/nawilny/aoc2022/day25/Day25.kt
+++ src/main/kotlin/com/nawilny/aoc2022/day25/Day25.kt
@@ -1,2 +1,48 @@
 package com.nawilny.aoc2022.day25
 
+import com.nawilny.aoc2022.common.Input
+
+fun main() {
+    val input = Input.readFileLinesNormalized("day25", "input.txt")
+    println(decimalToSnafu(input.sumOf { snafuToDecimal(it) }))
+}
+
+private fun snafuToDecimal(s: String): Long {
+    return s.reversed().withIndex().sumOf { pow5(it.index) * snafuDigitToDecimal(it.value) }
+}
+
+private fun decimalToSnafu(i: Long): String {
+    var d = i
+    var result = ""
+    while (d > 0) {
+        val s = (d + 2) / 5
+        val remain = d - (s * 5)
+        result += decimalDigitToSnafu(remain)
+        d = s
+    }
+    return result.reversed()
+}
+
+private fun pow5(i: Int) = (0 until i).fold(1L) { acc, _ -> acc * 5 }
+
+private fun snafuDigitToDecimal(c: Char): Long {
+    return when (c) {
+        '=' -> -2
+        '-' -> -1
+        '0' -> 0
+        '1' -> 1
+        '2' -> 2
+        else -> error("Unsupported digit '$c'")
+    }
+}
+
+private fun decimalDigitToSnafu(i: Long): Char {
+    return when (i) {
+        -2L -> '='
+        -1L -> '-'
+        0L -> '0'
+        1L -> '1'
+        2L -> '2'
+        else -> error("Cannot convert to snafu digit '$i'")
+    }
+}
